6f3902707603d9b6cc8de6fba1dd6728c09b49e6
nadirhbri <102730584+nadirhbri@users.noreply.github.com>
2023-01-28 18:39:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-wgttrd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-wgttrd)
---
+++ index.ts
@@ -0,0 +1,47 @@
+console.log('bonjour ! ');
+let anatomie: number;
+let biophysique: number;
+let embryologie: number;
+let cytologie: number;
+let biostatistique: number;
+let chimie: number;
+let physiologie: number;
+let biochimie: number;
+let somme: number;
+anatomie = 12 * 2;
+biophysique = 7.5 * 2;
+embryologie = 12 * 1;
+cytologie = 5.5 * 2;
+biostatistique = 5 * 2;
+chimie = 13 * 2;
+physiologie = 10 * 1;
+biochimie = 5.33 * 2;
+console.log('anatomie ');
+console.log(anatomie / 2);
+console.log('biophysique');
+console.log(biophysique / 2);
+console.log('embryologie');
+console.log(embryologie);
+console.log('cytologie');
+console.log(cytologie / 2);
+console.log('biostatistique');
+console.log(biostatistique / 2);
+console.log('chimie');
+console.log(chimie / 2);
+console.log('physiologie');
+console.log(physiologie);
+console.log('biochimie');
+console.log(biochimie / 2);
+somme =
+  anatomie +
+  biophysique +
+  embryologie +
+  cytologie +
+  biostatistique +
+  chimie +
+  physiologie +
+  biochimie;
+console.log('la somme les matieres est : ');
+console.log(somme);
+console.log('la moyenne générale de votre semestre est : ');
+console.log(somme / 14);
